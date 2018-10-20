be5643bdd631dad55b84745b203d5e11b3f070a3
jweiwu <wade30191@gmail.com>
2018-10-21 01:19:45 +0800

    2018.10.21 Modify the report model for nasa

--- nasa/models.py
+++ nasa/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Report(models.Model):
     _type = models.IntegerField()
-    _range = models.IntegerField()
+    # _range = models.IntegerField()
     longitude = models.FloatField()
     latitude = models.FloatField()
     postdate = models.DateTimeField()
-    remarks = models.CharField(max_length = 254)
+    remarks = models.CharField(max_length = 254, default=None, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add = True)
