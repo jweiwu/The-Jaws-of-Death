19b5b1d478c52533540f65f8f485f21eaac3ab92
jweiwu <wade30191@gmail.com>
2018-10-21 13:35:36 +0800

    Debug again

--- nasa/views.py
+++ nasa/views.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, status
 from .models import Report
@@ -19,7 +19,7 @@ def create_betch(request):
     return JsonResponse({'data': received_json_data}, status=status.HTTP_201_CREATED)
 
 def get_valid_data(lon, lat):
-    df = pd.DataFrame.from_records(Report.objects.values('longitude', 'latitude', 'value'))
+    df = pd.DataFrame.from_records(Report.objects.values())
     data = data_get.get_curr_data(df)
     data = data_get.get_section(data, float(lon), float(lat))
     return data
@@ -28,40 +28,43 @@ def get_valid_data(lon, lat):
 def get_wind(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.wind(data)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = data[['longitude', 'latitude', 'value']]
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 @csrf_exempt
 def get_rain(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.rain(data)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = data[['longitude', 'latitude', 'value']]
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 @csrf_exempt
 def get_temp(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.temp(data)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = data[['longitude', 'latitude', 'value']]
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 @csrf_exempt
 def get_wind_mean(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.wind(data)
-    data = np.mean(data.value)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = np.mean(data[['value']])
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 @csrf_exempt
 def get_rain_mean(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.rain(data)
-    data = np.mean(data.value)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = np.mean(data[['value']])
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 @csrf_exempt
 def get_temp_mean(request, lon, lat):
     data = get_valid_data(lon, lat)
     data = data_get.temp(data)
-    data = np.mean(data.value)
-    return JsonResponse({'data': pd.DataFrame(data).to_json(orient='index')})
+    data = np.mean(data[['value']])
+    return HttpResponse(data.to_json(orient='index'), content_type='application/json')
 
 
 class ReportViewSet(viewsets.ModelViewSet):
